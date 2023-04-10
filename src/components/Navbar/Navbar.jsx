eb67f6ab10bf8b13137c9898583086303a1714a3
sagorforu <freelancersagor18@gmail.com>
2023-04-10 13:18:14 +0600

    navbar all link done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            navbar
+        <div className='background-color py-5 lg:flex items-center justify-around'>
+            <Link to='/'><h2 className='text-[#1A1919] font-bold text-2xl'>Make Career</h2></Link>
+            <ul className='lg:flex text-[#757575] font-semibold'>
+          <li className='p-4'>
+            <NavLink
+              to='/'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className='p-4'>
+            <NavLink
+              to='/statistics'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Statistics
+            </NavLink>
+          </li>
+          <li className='p-4'>
+            <NavLink
+              to='/appliedJobs'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Applied Jobs
+            </NavLink>
+          </li>
+          <li className='p-4'>
+            <NavLink
+              to='/blog'
+              className={({ isActive }) => (isActive ? 'active' : 'default')}
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+            <Link to='/'><button className='btn-color px-3 py-2 rounded-md font-semibold text-sm text-white'>Star Applying</button></Link>
         </div>
     );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Statistics = () => {
     return (
-        <div>
+        <div className='my-container'>
             statistics
         </div>
     );
