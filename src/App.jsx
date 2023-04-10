7cf00fe2a72e175af2c828f739a44073d0dbe5d5
sagorforu <freelancersagor18@gmail.com>
2023-04-10 12:26:14 +0600

    all navbar route set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl text-red-600 font-bold underline">
-        Tailwind setup complete
-      </h1>
-      <h2>react route dom also added</h2>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
     </div>
   );
 };
