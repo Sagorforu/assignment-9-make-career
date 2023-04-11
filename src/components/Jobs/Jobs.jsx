3e63ec2cbb0d425606a1c32ce492ac0136df9849
sagorforu <freelancersagor18@gmail.com>
2023-04-11 19:27:29 +0600

    see more button added succesfully

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Job from '../Job/Job';
 
 const Jobs = () => {
     const jobs = useLoaderData();
+    const [data, setData] = useState((jobs.slice(0, 4)));
+
+    const fetchData = () => {
+        fetch('jobs.json')
+          .then(response => response.json())
+          .then(data => setData(data));
+      };
+
     return (
         <div className='py-24'>
             <h2 className='text-[#1A1919] text-center font-bold text-3xl lg:text-5xl'>Featured Jobs</h2>
             <p className='text-[#757575] text-center font-semibold my-6'>Explore thousands of job opportunities with all the information you need. Its your future</p>
             <div className='lg:px-36 px-2 lg:grid lg:grid-cols-2 lg:gap-6'>
                 {
-                    jobs.map(job => <Job
+                    data.map(job => <Job
                     key={job.id}
                     job={job}
                     ></Job>)
                 }
             </div>
             <div className='text-center'>
-            <Link to='/'><button className='btn-color text-center mt-10 px-5 py-3 rounded-md font-semibold text-base text-white'>See All Jobs</button></Link>
+            <Link to='/'><button onClick={fetchData} className='btn-color text-center mt-10 px-5 py-3 rounded-md font-semibold text-base text-white'>See All Jobs</button></Link>
             </div>
         </div>
     );
